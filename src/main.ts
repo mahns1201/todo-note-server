db80be1392bff59681e42743c58bc650e3454504
서민혁 <mahns1201@xingxingmobility.com>
2023-08-19 02:44:18 +0900

    Add: set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'debug', 'warn', 'error'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Dev To Do Notes')
+    .setDescription('Dev-To-Do-Notes API 문서')
+    .setVersion('1.0')
+    .addTag('dev-to-do-notes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
