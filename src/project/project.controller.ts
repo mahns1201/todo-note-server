c4736cba5c9449c2b06287590e3048ae565ed84b
mahns <mahns1201@gmail.com>
2023-11-15 21:44:20 +0900

    ADD: projects 임시 구현

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,9 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/jwt/auth.guard';
+import { UserService } from 'src/user/user.service';
 
 @Controller('project')
+@UseGuards(AuthGuard)
 @ApiTags('project')
 export class ProjectController {
-  constructor(private projectService: ProjectService) {}
+  constructor(
+    private projectService: ProjectService,
+    private userService: UserService,
+  ) {}
+
+  @Get('/github/list')
+  async getProjectsFromGithub(@Request() request) {
+    const { email, username } = request.user;
+
+    const {
+      item: { githubAccessToken },
+    } = await this.userService.findUser({ email });
+
+    const { items } = await this.projectService.getProjectsFromGithub(
+      githubAccessToken,
+      username,
+    );
+
+    return {
+      items,
+    };
+  }
+
+  @Get('/github')
+  async getProjectFromGithub(@Request() request) {
+    const { email, username } = request.user;
+
+    const {
+      item: { githubAccessToken },
+    } = await this.userService.findUser({ email });
+
+    const { item } = await this.projectService.getProjectFromGithub(
+      githubAccessToken,
+      username,
+    );
+
+    return {
+      item,
+    };
+  }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProjectEntity } from './entity/project.entity';
+import { Octokit } from 'octokit';
 
 @Injectable()
 export class ProjectService {
@@ -12,4 +13,64 @@ export class ProjectService {
 
     private httpService: HttpService,
   ) {}
+
+  async getProjectsFromGithub(githubAccessToken, username) {
+    const octokit = new Octokit({
+      auth: githubAccessToken,
+    });
+
+    const query = `query ($owner: String!, $pV2Number: Int!) {
+      user(login: $owner) {
+        projectsV2(first: $pV2Number) {
+          nodes {
+            id
+            title
+            number
+          }
+        }
+      }
+      }`;
+
+    const items = await octokit.graphql<{
+      user: {
+        projectsV2: {
+          nodes: {
+            id: string;
+            title: string;
+            number: string;
+          };
+        };
+      };
+    }>(query, { owner: username, pV2Number: 20 });
+
+    return { items };
+  }
+
+  async getProjectFromGithub(githubAccessToken, username) {
+    const octokit = new Octokit({
+      auth: githubAccessToken,
+    });
+
+    const query = `query ($owner: String!, $pV2Number: Int!) {
+      user(login: $owner) {
+        projectV2(number: $pV2Number) {
+          id
+        }
+      }
+      }`;
+
+    const item = await octokit.graphql<{
+      user: {
+        projectV2: {
+          id: string;
+        };
+      };
+    }>(query, { owner: username, pV2Number: 1 });
+
+    return { item };
+  }
 }
+
+// import { Octokit } from "octokit";
+// const octokit = new Octokit({ auth: "Here is your tokenTOKENtoken" });
+// getSomeProjectV2Data("LangLangBart", 5)
