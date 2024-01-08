e6e053a8d9c97b70a8c39b0b9cc38d6a48df1353
mahns <mahns1201@gmail.com>
2024-01-09 00:27:09 +0900

    ADD: 날짜 형식 in-out 지정

---
+++ src/util/date.ts
@@ -0,0 +1,19 @@
+export const convertIncomingDate = (incomingDate: string) => {
+  const now = new Date(
+    incomingDate.replace(/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)$/, '$1-$2-$3 $4:$5'),
+  );
+
+  return now;
+};
+
+export const convertOutgoingDate = (outgoingDate: Date) => {
+  const year = String(outgoingDate.getFullYear());
+  const month = String(outgoingDate.getMonth() + 1).padStart(2, '0');
+  const date = String(outgoingDate.getDate()).padStart(2, '0');
+  const hour = String(outgoingDate.getHours()).padStart(2, '0');
+  const minutes = String(outgoingDate.getMinutes()).padStart(2, '0');
+
+  const now = year + month + date + hour + minutes;
+
+  return now;
+};
