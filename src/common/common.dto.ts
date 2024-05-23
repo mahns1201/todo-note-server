74881e2dc39968bc4a698a9d2d8c40e22cbe64b7
서민혁 <mahns1201@gmail.com>
2024-05-24 02:48:12 +0900

    feat: add TokenDto

--- src/common/common.dto.ts
+++ src/common/common.dto.ts
@@ -1,6 +1,12 @@
 import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsDate, IsNotEmpty, IsNumber } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class BaseDto {
   @ApiProperty()
@@ -25,9 +31,21 @@ export class BaseDto {
   isDeleted: boolean;
 }
 
+export class TokenDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  accessToken: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  githubAccessToken: string;
+}
+
 export class ResDto<T> {
   @ApiProperty({ description: 'http 상태 코드' })
-  httpStatus: HttpStatus;
+  statusCode: HttpStatus;
 
   @ApiProperty({ description: 'api 응답 메시지' })
   message: string;
